c59ffbdcb31db8366666660ee14b0c79615c8eab
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2023-10-20 09:57:20 +0200

    contrast calc

---
+++ contrast.py
@@ -0,0 +1,19 @@
+
+def michelson_contrast(l_min, l_max):
+    return abs((l_max - l_min)) / (l_max + l_min)
+
+if __name__ == "__main__":
+    step_size = 3
+
+    background = 0
+    foreground = 255
+
+    dec = (255 / 2) / step_size
+
+    print("\nBackground, Foreground, Michelson Contrast \n---")
+
+    for i in range(step_size):
+        back, fore = round(background), round(foreground)
+        print(f"{back}, {fore}, {michelson_contrast(back, fore)} \n---")
+        background += dec
+        foreground -= dec
