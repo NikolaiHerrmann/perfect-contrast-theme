c637a53203919a458f87950874dab6848981762f
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2023-11-09 13:52:50 +0100

    readme update and plot

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Perception for Visual Computing Contrast Experiments
+
+## RQ
+*Does the luminance contrast of a coding editor theme affect code comprehension?*
+
+## Dependencies
+
+[VS Code](https://github.com/microsoft/vscode/releases/tag/1.83.0) version 1.83.0 was used for all experiments.
+
+Install requirements as follows (tested on Python 3.11.6):
+```
+pip3 install -r requirements.txt
+```
+
+Experiments were carried out on Fedora 38
+
+## File Structure
+
+### Experiment Files 
+
+- [`background_script.py`](background_script.py): main background script switching themes and timing participants
+
+- [`contrast.py`](contrast.py): determine gray-scale values given Michelson contrast
+
+- [`trial_1_func.py`](trial_1_func.py): comprehension questions for first coding task
+
+- [`trial_2_func.py`](trial_2_func.py): comprehension questions for second coding task
+
+- [`trial_3_func.py`](trial_3_func.py): comprehension questions for third coding task
+
+### Data
+
+[`data`](data/): participant results
+
+### Analysis
+
+[`results.py`](results.py): main experimental results (response times and response correctness)
+
+[`first_question_plot.py`](first_question_plot.py): visually highlights method error of not showing example question beforehand
--- first_question_plot.py
+++ first_question_plot.py
@@ -17,21 +17,19 @@ def subplot():
 
         for i in range(3):
             idx = i * 6
-            times = df.iloc[idx:idx+6, :]["Completion Time"].to_numpy()
-            y = np.zeros((6, 1))
-            axs[count].scatter(times, y)
-            axs[count].scatter([times[0]], [0])
+            times = df.iloc[idx:idx+6, :]["Completion Time"].to_numpy().tolist()
+            axs[count].scatter([times.pop(0)], [0], label="Comprehension Question 1", color="tab:orange")
+            axs[count].scatter(times, np.zeros((len(times), 1)), label="Comprehension Questions 2 to 6", color="tab:blue")
             axs[count].set_yticks([])
-            axs[count].set_ylabel(f"Participant {j + 1} \n Task {i + 1}", rotation=0)
+            axs[count].set_ylabel(f"$\\it{{Participant {j + 1}}}$\nTask {i + 1}", rotation=0)
             axs[count].yaxis.set_label_coords(-0.11, 0.05)
             count += 1
 
     fig.supxlabel("Completion Time (Seconds)", y=-0.03)
-    plt.suptitle("Comparing Completion Time of First Theme Presented", y=1.02)
+    plt.suptitle("Completion Time of First Comprehension Question \n Compared to Subsequent Ones", y=1.06)
+    plt.legend(loc="lower center", bbox_to_anchor=(0.5, -4.5))
 
     plt.savefig(os.path.join(FIGURE_PATH, "first_theme_times.pdf"), bbox_inches="tight")
-    plt.show()
-
 
 if __name__ == "__main__":
     subplot()
